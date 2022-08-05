290baf6f2b8d03e8038b74d089acdd1840f9c5a7
r-petes <rachel.peterson.5683@gmail.com>
2022-08-04 19:39:42 -0600

    began to include logic for search results

--- app.js
+++ app.js
@@ -36,8 +36,13 @@ app.post('/searchresults', function (req, res) {
         let result;
         requester.on("message", function (reply) {
         result = reply.toString();
-
-        findmatches(result) 
+            console.log(result)
+        if(findmatches(result, searchRadius) == true) {
+            res.sendFile(__dirname + '/searchresults.html');
+        }
+        else {
+            res.sendFile(__dirname + '/searchfailed.html');
+        }
 
         });
 
@@ -50,7 +55,7 @@ app.post('/searchresults', function (req, res) {
         requester.close();
         });
         
-    res.sendFile(__dirname + '/searchresults.html');
+    
 });
 
 var server = app.listen(3000, function () {
@@ -58,7 +63,7 @@ var server = app.listen(3000, function () {
 });
 
 
-function findmatches(latlongresult){
+function findmatches(latlongresult, searchRadius){
 
     // Read data into variables. 
     // For each park, check distance
@@ -67,27 +72,34 @@ function findmatches(latlongresult){
     var long1 = parseFloat(splitString[1])
 
 
-    // JSON file reading adapted from Fetch API documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    fetch('https://developer.nps.gov/api/v1/parks', {
-    headers: {
-        'accept': 'application/json',
-        'Authorization':'dqWgodPXhWoWgjtfThhrw6RHocpkb79IF7M3d2BH'
-    }})
-        .then((response) => console.log(response))
-        .then((data) => console.log(data));
+    // // JSON file reading adapted from Fetch API documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    // fetch('https://developer.nps.gov/api/v1/parks', {
+    // headers: {
+    //     'accept': 'application/json',
+    //     'Authorization':'dqWgodPXhWoWgjtfThhrw6RHocpkb79IF7M3d2BH'
+    // }})
+    //     .then((response) => console.log(response))
+    //     .then((data) => console.log(data));
 
-    var lat2 = 0 
-    var long2 = 0
+    var lat2 = -105.2838511 
+    var long2 = 40.0454736
 
-    checkdistance(lat1, long1, lat2, long2) 
+    if(checkdistance(lat1, long1, lat2, long2, searchRadius) == true) {
+        return true;
+    }
 
 };
 
-function checkdistance(lat1, long1, lat2, long2){
+function checkdistance(lat1, long1, lat2, long2, searchRadius){
   
     // Haversine formula library from https://www.npmjs.com/package/haversine-distance 
     const a = [lat1, long1]
     const b = [lat2, long2]
      
-    distanceInMiles = console.log(haversine(a, b)/1609.344) // distance between points in miles
+    distanceInMiles = haversine(a, b)/1609.344 // distance between points in miles
+
+    if(distanceInMiles < searchRadius) {
+        return true;
+    }
+
 };
